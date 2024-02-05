de4e5c0983a152b04714ca5c241a68e610c77d1f
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-02-05 11:05:55 +0530

    DOM

--- 10_DOM.js
+++ 10_DOM.js
@@ -1,4 +1,9 @@
 // DOM - Document Object Model
-// console.log(document) in console
-console.dir(document)
+//console.log(window) in console
+// console.log(document)
+//console.dir(document) -> Show all elements
 //window.console.log("hello");
+
+document.getElementById('firstHeading')
+
+document.getElementById('firstHeading').innerHTML = "<h1>Hello World</h1>" //-> It change the webpage this is called DOM Manipulation
