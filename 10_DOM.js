40b980bbba603ae96960ffee6ad63dae455822ed
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-03-10 09:33:30 +0530

    deleted

--- 10_DOM.js
+++
@@ -1,9 +0,0 @@
-// DOM - Document Object Model
-//console.log(window) in console
-// console.log(document)
-//console.dir(document) -> Show all elements
-//window.console.log("hello");
-
-document.getElementById('firstHeading')
-
-document.getElementById('firstHeading').innerHTML = "<h1>Hello World</h1>" //-> It change the webpage this is called DOM Manipulation
