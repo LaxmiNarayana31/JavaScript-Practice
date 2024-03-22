c74e8d4eb874cffa9ccfa9ded8914fc99129f14f
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-03-22 08:16:03 +0530

    JS Prototypes

---
+++ 13_OOPS/Prototypes.js
@@ -0,0 +1,19 @@
+/* Prototypes are the mechanism by which JavaScript objects inherit features from one another.
+
+All JavaScript objects inherit properties and methods from a prototype.
+- Date objects inherit from Date.prototype
+- Array objects inherit from Array.prototype
+
+The Object.prototype is on the top of the prototype inheritance chain:
+
+Date objects, Array objects, and Person objects inherit from Object.prototype.  
+
+Array ------> Object ------> null 
+String ------> Object ------> null
+Function ------> Object ------> null
+
+Everything in JavaScript is an Object 
+*/
+
+// 7:26:15
+ 
