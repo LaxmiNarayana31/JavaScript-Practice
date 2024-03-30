9d3ae5a0df756bd869ed6caaf99eb3d46f79f645
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-03-30 22:56:09 +0530

    About Constructors in JS

--- 13_OOPS/MyClasses.js
+++ 13_OOPS/MyClasses.js
@@ -18,5 +18,23 @@ const user1 = new User("Shiva_31", "Shiva@gmail.com", "Shiva@345")
 console.log(user1.encryptPassword()); // output: Shiva@345abc
 console.log(user1.changeUsername()) // output: SHIVA_31
 
-// 8:20:49
 
+
+
+// behind scene
+function user(username, email, password) {
+    this.username = username
+        this.email = email
+        this.password = password
+}
+    
+User.prototype.encryptPassword = function () {
+        return `${this.password}abc`
+}
+User.prototype.changeUsername = function () {
+        return `${this.password.toUpperCase()}`
+}
+
+const user2 = new User("Sumit", "sumit12@gmail.com", "sumit@123")
+console.log(user2.encryptPassword()); // output: sumit@123abc
+console.log(user2.changeUsername()); // output: SUMIT@123
